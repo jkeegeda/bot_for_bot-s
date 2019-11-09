b00f15aef0363a1233c350f9a718ae7e50ece654
jkeegeda <55329706+jkeegeda@users.noreply.github.com>
2019-11-10 02:35:16 +0300

    Update botforbot's.py

--- botforbot's.py
+++ botforbot's.py
@@ -6,8 +6,8 @@ import time
 import os
 
 
-mytokenvk = os.environ.get('TOKEN_VK')
-vk_session = vk_api.VkApi(token=mytokenvk)
+#mytokenvk = os.environ.get('TOKEN_VK')
+vk_session = vk_api.VkApi(token="0c0151147883a61fcc4cf73fd9255c0d53045071fb0697f8995c14294abfb1add34f73f7dd754112452bd")
 longpull = VkLongPoll(vk_session)
 vk = vk_session.get_api()
 
